c4f7eebbd237158f55555ea58a0bb57d4d3d280c
Muhamad Faturrahman <114564154+MuhamadFaturrahman@users.noreply.github.com>
2024-09-10 09:51:38 +0700

    Fixing and Adding Animation Between Sections

--- src/app/components/ExperiencesSection.tsx
+++ src/app/components/ExperiencesSection.tsx
@@ -5,27 +5,51 @@ import imageFirst from '../assets/01_pic.jpg';
 
 function ExperiencesSection() {
   return (
-    <section className='scroll-px-20' id='#experiences'>
+    <section id='#experiences'>
         {/* Title Section */}
-        <div className="grid grid-cols-1 lg:grid-cols-12 flex-auto space-y-5">
-            <h2 className='lg:col-span-12 place-self-center text-center px-4 w-full lg:w-max bg-gradient-to-br from-[#054d7d] to-[#18A4E0] inline-block rounded-md  text-[#FAF8F0] text-xl lg:text-4xl font-bold'>
+        <div className="md:grid md:grid-cols-12 gap-2 items-center py-0 lg:gap-8 lg:px-20 lg:py-10 space-y-5 lg:space-y-0">
+            <h2 className='col-span-12 place-self-center text-center w-full lg:w-max bg-gradient-to-br from-[#054d7d] to-[#18A4E0] inline-block rounded-md  text-[#FAF8F0] text-2xl lg:text-4xl font-bold'>
                 The Journeys That Makes Me A Person
             </h2>
-            {/* Image 1 Section */}
-            <div className=" lg:col-span-4 flex justify-center lg:justify-start my-5 lg:my-0">
-            <Image
-                src = {imageFirst}
-                alt="Stock photo of Graphic Designer"
-                className='absolute'
-                width={400}
-                height={400}
-                />        
-        </div>
-        {/* Desc 1 Section */}
-        <div className="col-span-1 lg:col-span-8 lg:pl-10 px-5 lg:px-0 mx-5 lg:mx-0">
-        <h3 className="text-2xl font-bold">Hello</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quae eos quidem asperiores alias amet cupiditate voluptatum, quo molestiae laborum magnam excepturi tenetur fugiat architecto repellendus a dignissimos? Labore, corporis!</p>
-        </div>
+            {/* Experience Section 1 */}
+            <div className="col-span-12 lg:col-span-5 place-self-center lg:place-self-start">
+              <Image
+                  src = {imageFirst}
+                  alt="Stock photo of Graphic Designer"
+                  className='rounded-md '
+                  />
+            </div> 
+          
+          <div className="md:col-span-7">
+            <h3 className="text-xl font-bold">Hello</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quae eos quidem asperiores alias amet cupiditate voluptatum, quo molestiae laborum magnam excepturi tenetur fugiat architecto repellendus a dignissimos? Labore, corporis!</p>
+          </div>
+          {/* Experience Section 2 */}
+          <div className="md:col-span-7">
+            <h3 className="text-xl font-bold">Hello</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quae eos quidem asperiores alias amet cupiditate voluptatum, quo molestiae laborum magnam excepturi tenetur fugiat architecto repellendus a dignissimos? Labore, corporis!</p>
+          </div>
+          <div className="col-span-12 lg:col-span-5 place-self-center lg:place-self-start">
+              <Image
+                  src = {imageFirst}
+                  alt="Stock photo of Graphic Designer"
+                  className='rounded-md '
+                  />
+          </div>
+          {/* Experience Section 1 */}
+          <div className="col-span-12 lg:col-span-5 place-self-center lg:place-self-start">
+              <Image
+                  src = {imageFirst}
+                  alt="Stock photo of Graphic Designer"
+                  className='rounded-md '
+                  />
+            </div> 
+          
+          <div className="md:col-span-7">
+            <h3 className="text-xl font-bold">Hello</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quae eos quidem asperiores alias amet cupiditate voluptatum, quo molestiae laborum magnam excepturi tenetur fugiat architecto repellendus a dignissimos? Labore, corporis!</p>
+          </div>
+          
         </div>
         
     </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,39 @@
+"use client"
+import { motion } from "framer-motion";
 import ExperiencesSection from "./components/ExperiencesSection";
 import HeaderSection from "./components/HeaderSection";
 import NavbarSection from "./components/NavbarSection";
 
+// Framer Motion animation variants for sections
+const sectionVariants = {
+  hidden: { opacity: 0, y: 50 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.6, ease: "easeOut" } },
+};
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col container mx-auto px-10 ">
-      <NavbarSection/>
-      <div className="container mt-24 lg:mt-36 py-4 space-y-10">
-        <HeaderSection/>
-        <ExperiencesSection/>
-      </div>
-      
+    <main className="flex min-h-screen flex-col container mx-auto px-10">
+      <NavbarSection />
+      <motion.div
+        className="container mt-24 lg:mt-36 py-4"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.2 }} // Adjust how much of the section should be in view to trigger animation
+        variants={sectionVariants}
+      >
+        <HeaderSection />
+      </motion.div>
+
+      {/* Wrapping ExperiencesSection in a motion.div for scroll animation */}
+      <motion.div 
+        className="container py-4"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.2 }}
+        variants={sectionVariants}
+      >
+        <ExperiencesSection />
+      </motion.div>
     </main>
   );
 }
